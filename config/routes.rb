73f7dd825c0e8a336e3bcaa4a4d9f99f89ead892
alfredopl <aalfredoplz@gmail.com>
2020-12-15 12:31:27 -0300

    routes default pages one and get pages three

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/one,'
+  get 'pages/one'
   get 'pages/two'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'pages/three'
+  root 'pages#one'
 end
